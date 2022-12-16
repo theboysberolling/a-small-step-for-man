ce5b3927bdef0661598160b4f65c50c1c2c00006
JOKERadaw <valerioman05@gmail.com>
2022-12-16 20:55:41 +0100

    added settings
    i added the settings button for now the slider doesnt work

--- a small step for man-unity/Assets/scripts/backgroundLoop.cs
+++ a small step for man-unity/Assets/scripts/backgroundLoop.cs
@@ -60,8 +60,7 @@ public class backgroundLoop : MonoBehaviour
     }
     void Update()
     {
-        score = textscore.currentscore;
-        
+
         Vector3 velocity = Vector3.zero;
         Vector3 desiredPosition = transform.position + new Vector3(scrollSpeed, 0, 0);
         Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, 0.3f);
